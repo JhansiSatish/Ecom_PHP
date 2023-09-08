3ffce56cad1ca2a51a60c7b348d44734357c1228
JhansiSatish <srijhansi.chowdary@gmail.com>
2023-09-08 12:09:58 -0700

    vide09

--- functions/myfunctions.php
+++ functions/myfunctions.php
@@ -1,4 +1,10 @@
 <?php
+include('../config/dbcon.php');
+ function getAll($table)
+ { global $con; 
+   $sql="select * from $table";
+   return $result= mysqli_query($con,$sql);
+ }
  function redirect($url,$message){
     $_SESSION['message']=$message;
     header('location:'.$url);
